478af07c68124fbfb757f1325acb06dd2241ff5b
Leandro Serra <leandro.serra@lambdaclass.com>
2024-11-21 14:57:06 -0300

    Add opcodes, registers, load program to memory

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod lc3_vm;
+use lc3_vm::virtual_machine::VM;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut vm = VM::default();
+    vm.load_program("./2048.obj")?;
+    Ok(())
+}
